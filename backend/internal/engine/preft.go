9fe636185647c452226232d4ab75c606fb482465
LiamBlanton17 <liamblanton@gmail.com>
2026-01-27 16:34:21 -0800

    preft WIP #1

---
+++ backend/internal/engine/preft.go
@@ -0,0 +1,11 @@
+package engine
+
+import "fmt"
+
+/*
+This file holds the Preft test, which can be called via the command line.
+This is used for validating the engine's correctness when it comes to move generation and searching
+*/
+func Preft() {
+	fmt.Println("Starting PREFT test.")
+}
