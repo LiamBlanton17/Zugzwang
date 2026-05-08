105c45e6f78055ce23b90a1402dbde3849295cc9
LiamBlanton17 <liamblanton@gmail.com>
2026-05-08 11:32:50 -0700

    clean repo

--- src/cmd/engine/main.go
+++ src/cmd/engine/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"zugzwang/internal/engine"
+	"zugzwang/engine"
 )
 
 /*
--- src/cmd/magic/main.go
+++ src/cmd/magic/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"zugzwang/internal/engine"
+	"zugzwang/engine"
 
 	"fmt"
 	"math/bits"
